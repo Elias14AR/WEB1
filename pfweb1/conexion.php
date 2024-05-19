79cf19b60a57d1f7f5db0a683a2627f0146e1b80
Elias14AR <119818156+Elias14AR@users.noreply.github.com>
2024-05-19 15:08:39 -0600

    5TO commit
    Correccion de errores inicio de sesion

--- pfweb1/conexion.php
+++ pfweb1/conexion.php
@@ -1,15 +1,20 @@
 <?php
 
-$user_db="if0_36150760";
-$pass_db="EhWU6kFfQWUvuJ";
-$db_name="if0_36150760_pweb";
-$host_db="sql101.infinityfree.com";
-$conexion = new mysqli($host_db,$user_db,$pass_db,$db_name);
+$archivo_actual = basename($_SERVER['PHP_SELF']);
 
-if($conexion-> conect_error){
-    die("La conexion peto".$conexion->conect_error);
-}else{
-    echo "conexion con exito";
+$user_db = "if0_36150760";
+$pass_db = "EhWU6kFfQWUvuJ";
+$db_name = "if0_36150760_pweb";
+$host_db = "sql101.infinityfree.com";
+$conexion = new mysqli($host_db, $user_db, $pass_db, $db_name);
+
+if ($conexion->connect_error) {
+    die("La conexión falló: " . $conexion->connect_error);
+} else {
+    // Si no estamos en el archivo read.php, mostrar el mensaje
+    if ($archivo_actual !== "read.php") {
+        echo "Conexión realizada con éxito";
+    }
 }
 
 ?>
